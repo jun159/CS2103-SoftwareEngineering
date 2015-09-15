f2037a0f5688951d619b14fc583ac66f820ce284
jun159 <baojun159@gmail.com>
2015-09-16 00:56:40 +0800

    Changed whole TextBuddy to OOP style

--- CE2/src/TextBuddyTester.java
+++ CE2/src/TextBuddyTester.java
@@ -9,42 +9,26 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TextBuddyTester {
-
-	private static final String INPUT_FILE_NAME = "mytestFile.txt";
 	
-	private static final String COMMAND_ADD = "add";
-	private static final String COMMAND_DISPLAY = "display";
-	private static final String COMMAND_SORT = "sort";
-	private static final String COMMAND_SEARCH = "search";
+	Parser parser;
+	Storage storage;
 	
-	private static final String SEARCH_WORD = "East";
-	
-	private static final String TEXT_1 = "Potong Pasir";
-	private static final String TEXT_2 = "East Coast";
-	private static final String TEXT_3 = "Ang Mo Kio";
-	private static final String TEXT_4 = "Jurong East";
-	private static final String TEXT_5 = "Bishan";
-	private static final String TEXT_6 = "Chua Chu Kang";
-	
-	private static final String NUM_1 = "1. ";
-	private static final String NUM_2 = "2. ";
-	private static final String NUM_3 = "3. ";
-	private static final String NUM_4 = "4. ";
-	private static final String NUM_5 = "5. ";
-	
-	
-	private static final int NUMBER_OF_TEXTS_CONTAINING_WORD_EAST = 2;
+	private void initialize() throws IOException {
+		parser = new Parser();
+		storage = new Storage();
+	}
 	
 	@Before
 	public void createFile() throws FileNotFoundException, IOException {
-		TextBuddy.setFileName(INPUT_FILE_NAME);
-		TextBuddy.initializeFile();
+		initialize();
 		
-		TextBuddy.executeCommand(COMMAND_ADD, TEXT_1);
-		TextBuddy.executeCommand(COMMAND_ADD, TEXT_2);
-		TextBuddy.executeCommand(COMMAND_ADD, TEXT_3);
-		TextBuddy.executeCommand(COMMAND_ADD, TEXT_4);
-		TextBuddy.executeCommand(COMMAND_ADD, TEXT_5);
+		storage.createNewFile(Constant.INPUT_FILE_NAME);
+		
+		parser.executeCommand(Constant.COMMAND_ADD, Constant.TEXT_1);
+		parser.executeCommand(Constant.COMMAND_ADD, Constant.TEXT_2);
+		parser.executeCommand(Constant.COMMAND_ADD, Constant.TEXT_3);
+		parser.executeCommand(Constant.COMMAND_ADD, Constant.TEXT_4);
+		parser.executeCommand(Constant.COMMAND_ADD, Constant.TEXT_5);
 	}
 	
 	/*
@@ -55,9 +39,11 @@ public class TextBuddyTester {
 	
 	@Test
 	public void testSortList() throws IOException {
-		TextBuddy.executeCommand(COMMAND_SORT, null);
-		assertEquals(NUM_1 + TEXT_3 + NUM_2 + TEXT_5 + NUM_3 + TEXT_2 + NUM_4 
-				+ TEXT_4 + NUM_5 + TEXT_1, TextBuddy.executeCommand(COMMAND_DISPLAY, null));
+		parser.executeCommand(Constant.COMMAND_SORT, null);
+		assertEquals(Constant.NUM_1 + Constant.TEXT_3 + Constant.NUM_2 + 
+				Constant.TEXT_5 + Constant.NUM_3 + Constant.TEXT_2 + Constant.NUM_4 
+				+ Constant.TEXT_4 + Constant.NUM_5 + Constant.TEXT_1, 
+				parser.executeCommand(Constant.COMMAND_DISPLAY, null));
 	}
 	
 	/*
@@ -68,21 +54,22 @@ public class TextBuddyTester {
 	
 	@Test
 	public void testSearchNullInput() throws IOException {
-		assertEquals(TextBuddy.ERROR_NULL_SEARCH_INPUT, 
-				TextBuddy.executeCommand(COMMAND_SEARCH, null));
+		assertEquals(Constant.ERROR_NULL_SEARCH_INPUT, 
+				parser.executeCommand(Constant.COMMAND_SEARCH, null));
 	}
-	
 	
 	@Test
 	public void testSearchEmptyResult() throws IOException {
-		assertEquals(String.format(TextBuddy.MESSAGE_SEARCH_IS_EMPTY, TEXT_6, INPUT_FILE_NAME), 
-				TextBuddy.executeCommand(COMMAND_SEARCH, TEXT_6));
+		assertEquals(String.format(Constant.MESSAGE_SEARCH_IS_EMPTY, 
+				Constant.TEXT_6, Constant.INPUT_FILE_NAME), 
+				parser.executeCommand(Constant.COMMAND_SEARCH, Constant.TEXT_6));
 	}
 	
 	@Test
 	public void testSearchResult() throws IOException {
-		assertEquals(String.format(TextBuddy.MESSAGE_SEARCH_TEXT, NUMBER_OF_TEXTS_CONTAINING_WORD_EAST , 
-				SEARCH_WORD, INPUT_FILE_NAME) + NUM_1 + TEXT_2 + NUM_2 + TEXT_4,
-				TextBuddy.executeCommand(COMMAND_SEARCH, SEARCH_WORD));
+		assertEquals(String.format(Constant.MESSAGE_SEARCH_TEXT, Constant.NUMBER_OF_TEXTS_CONTAINING_WORD_EAST, 
+				Constant.SEARCH_WORD, Constant.INPUT_FILE_NAME) + Constant.NUM_1 + 
+				Constant.TEXT_2 + Constant.NUM_2 + Constant.TEXT_4,
+				parser.executeCommand(Constant.COMMAND_SEARCH, Constant.SEARCH_WORD));
 	}
 }
