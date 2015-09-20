50ed690ec565ed05f5d72c00fb7e005d429f81de
jun159 <baojun159@gmail.com>
2015-09-21 06:21:08 +0800

    Declare attribute for magic strings and numbers

--- TankTaskStorage/Storage/StorageJSON.java
+++ TankTaskStorage/Storage/StorageJSON.java
@@ -1,78 +1,111 @@
 package Storage;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.TypeFactory;
 
-import Task.Category;
 import Task.CategoryWrapper;
-import Task.Subtask;
 import TaskTask.Task;
 
 public class StorageJSON {
 	
-	private JSONObject category;
-	private JSONObject task;
+	/* The following represents the types of tasks */
+	private static final String TYPE_TASK = "task";
+	private static final String TYPE_FLOAT = "floatTask";
+	private static final String TYPE_EVENT = "event";
 	
-	private JSONArray categoryList;
-	private JSONArray taskList;
+	/* The following represents the key of each category attributes */
+	private static final String CATEGORY_KEY_NAME = "categoryName";
 	
-	private JSONParser parser;
+	/* The following represents the key of each task attributes */
+	private static final String TASK_KEY_NAME = "name";
+	private static final String TASK_KEY_DESCRIPTION = "description";
+	private static final String TASK_KEY_STARTDATE = "startDate";
+	private static final String TASK_KEY_ENDDATE = "endDate";
+	private static final String TASK_KEY_STARTTIME = "startTime";
+	private static final String TASK_KEY_ENDTIME = "endTime";
+	private static final String TASK_KEY_REMINDER = "reminder";
+	private static final String TASK_KEY_PRIORITY = "priority";
+	private static final String TASK_KEY_ISDONE = "isDone";
+	
+	private static final String UNCHECKED = "unchecked";
+	private static final int EMPTY = -1;
 
-	public StorageJSON() throws FileNotFoundException, IOException { 
-		category = new JSONObject();
-		task = new JSONObject();
-		taskList = new JSONArray();
-		parser = new JSONParser();
+	public StorageJSON()  { 
+		
 	}
 	
-	// Retrieves all tasks from file
-    public ArrayList<CategoryWrapper> getAllTasksFromFile(String jsonString) throws JsonParseException, JsonMappingException, IOException {
+	private String checkTaskType(Task task) {
+		if (task.getStartTime() == EMPTY && task.getEndTime() != EMPTY) {
+			return TYPE_TASK;
+		} else if (task.getStartTime() == EMPTY && task.getEndTime() == EMPTY) {
+			return TYPE_FLOAT;
+		} else {
+			return TYPE_EVENT;
+		}
+	}
+	
+	// Retrieve all tasks from file
+    public ArrayList<CategoryWrapper> getAllTasksFromFile(String jsonString) 
+    		throws JsonParseException, JsonMappingException, IOException {
     	ObjectMapper mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        ArrayList<CategoryWrapper> allTasks = mapper.readValue(jsonString, new TypeReference<ArrayList<CategoryWrapper>>() {});
+        ArrayList<CategoryWrapper> allTasks = 
+        		mapper.readValue(jsonString, new TypeReference<ArrayList<CategoryWrapper>>() {});
     	return allTasks;
     }
     
-    @SuppressWarnings("unchecked")
-	public JSONArray addNewCategory(String categoryName) {
-    	Task newTask = new Task("Do homework", "Do it now", 5, 123, "School", false);
-    	
+    // Add new category if category does not exist
+    @SuppressWarnings(UNCHECKED)
+	public JSONArray addNewCategory(String categoryName, Task newTask) {
+
     	JSONArray allCategories = new JSONArray();
     	JSONObject categoryObject = new JSONObject();
-
-    	JSONObject typesOfTasks = new JSONObject();
-    	JSONArray taskArray = new JSONArray();
-
+    	JSONObject taskType = new JSONObject();
+    	JSONArray allTaskTypes = new JSONArray();
     	JSONObject taskObject = convertTaskToJSON(newTask);
 
-    	// Each type task array - task, floatTask or event contains 2 taskObject
-    	taskArray.add(taskObject);
-    	taskArray.add(taskObject);
+    	// Each type task array - task, floatTask or event contains taskObject
+    	allTaskTypes.add(taskObject);
     	
-    	// All type tasks
-    	typesOfTasks.put("floatTask", taskArray);
-    	typesOfTasks.put("task", taskArray);
-    	typesOfTasks.put("event", taskArray);
+    	// Check type of task to add under
+    	taskType.put(checkTaskType(newTask), allTaskTypes);
     	
-    	categoryObject.put(categoryName, typesOfTasks);
+    	categoryObject.put(categoryName, taskType);
+    	categoryObject.put(CATEGORY_KEY_NAME, categoryName);
+    	
+    	allCategories.add(categoryObject);
+    	//allCategories.add(categoryObject);
+    	
+    	return allCategories;
+    }
+    
+    // Add new task into existing category
+    @SuppressWarnings(UNCHECKED)
+	public JSONArray addNewTask(String categoryName, Task newTask) {
+
+    	JSONArray allCategories = new JSONArray();
+    	JSONObject categoryObject = new JSONObject();
+    	JSONObject taskType = new JSONObject();
+    	JSONArray allTaskTypes = new JSONArray();
+    	JSONObject taskObject = convertTaskToJSON(newTask);
+
+    	// Each type task array - task, floatTask or event contains taskObject
+    	allTaskTypes.add(taskObject);
+    	
+    	// Check type of task to add under
+    	taskType.put(checkTaskType(newTask), allTaskTypes);
+    	
+    	categoryObject.put(categoryName, taskType);
     	categoryObject.put("categoryName", categoryName);
     	
     	allCategories.add(categoryObject);
@@ -82,17 +115,19 @@ public class StorageJSON {
     }
     
     // Convert Task to JSON
-    @SuppressWarnings("unchecked")
+    @SuppressWarnings(UNCHECKED)
 	public JSONObject convertTaskToJSON(Task targetTask) {
-    	task.put("name", targetTask.getName());
-    	task.put("description", targetTask.getDescription());
-    	task.put("startDate", targetTask.getStartDate());
-    	task.put("endDate", targetTask.getEndDate());
-    	task.put("startTime", targetTask.getStartTime());
-    	task.put("endTime", targetTask.getEndTime());
-    	task.put("reminder", targetTask.getReminder());
-    	task.put("priority", targetTask.getPriority());
-    	task.put("isDone", targetTask.getDone());
+    	JSONObject task = new JSONObject();
+    
+    	task.put(TASK_KEY_NAME, targetTask.getName());
+    	task.put(TASK_KEY_DESCRIPTION, targetTask.getDescription());
+    	task.put(TASK_KEY_STARTDATE, targetTask.getStartDate());
+    	task.put(TASK_KEY_ENDDATE, targetTask.getEndDate());
+    	task.put(TASK_KEY_STARTTIME, targetTask.getStartTime());
+    	task.put(TASK_KEY_ENDTIME, targetTask.getEndTime());
+    	task.put(TASK_KEY_REMINDER, targetTask.getReminder());
+    	task.put(TASK_KEY_PRIORITY, targetTask.getPriority());
+    	task.put(TASK_KEY_ISDONE, targetTask.getDone());
     	
     	return task;
     }
